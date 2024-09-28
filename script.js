86ae4779af9303cc1d7a17aaf2da11f8b0a5b702
amayl4 <amayl4@outlook.com>
2024-09-28 15:46:24 +0100

    added comments to code

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+// Assign the id="" variables to JS variables
 let signupBtn = document.getElementById("signupBtn");
 let loginBtn = document.getElementById("loginBtn");
 let nameField = document.getElementById("nameField");
 let title = document.getElementById("title");
 
 loginBtn.onclick = function() {
-    nameField.style.maxHeight = "0";
-    title.innerHTML = "Login";
-    signupBtn.classList.add("disable");
-    loginBtn.classList.remove("disable")
+    nameField.style.maxHeight = "0"; // gets rid of the name field
+    title.innerHTML = "Login"; // changes the title from 'Sign Up' to 'Login'
+    signupBtn.classList.add("disable"); // unhighlights the signup button
+    loginBtn.classList.remove("disable") // highlights the login button
 }
 
+
 signupBtn.onclick = function() {
-    nameField.style.maxHeight = "60px";
-    title.innerHTML = "Sign Up";
-    signupBtn.classList.remove("disable");
-    loginBtn.classList.add("disable")
+    nameField.style.maxHeight = "60px"; // adds the name field
+    title.innerHTML = "Sign Up"; // changes the title from 'Login' to 'Sign Up'
+    signupBtn.classList.remove("disable"); // highlights the signup button
+    loginBtn.classList.add("disable") // unhighlights the signup button
 }
