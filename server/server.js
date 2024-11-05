c365ba2cf9ae837a65aa99855168d8b8d8e96751
amayl4 <amayl4@outlook.com>
2024-11-05 11:59:31 +0000

    idk

--- server/server.js
+++ server/server.js
@@ -7,14 +7,16 @@ const app = express() // app thingy in question (server object)
 app.use(cors()) // allow all ports
 app.use(express.json()) // to parse the JSON body
 
+// sends 'Pong' if successful connection
 app.get('/ping', (req, res) => res.send('Pong'))
+
 
 app.post('/signup', (req, res) => {
   const data = req.body;
 
 
-  res.send({ hello: 'world' })
+  res.send({ hello: 'world' }) // sends hello world to the console of server running
 })
 
-app.listen(4000)
-console.log('Server running')
+app.listen(4000) // listens from port 4000
+console.log('Server running') // sends message to the console upon running
