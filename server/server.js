24c0cdfbc6bc020618a6f0694f00dc8d7f22e19f
amayl4 <amayl4@outlook.com>
2024-11-06 01:16:58 +0000

    just need to call the function onsubmit properly

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express') // import express for the app thingy
-const cors = require('cors')
+const cors = require('cors');
 const app = express() // app thingy in question (server object)
+
+// define the CSV file variables
+let users = 'users.csv';
+let products = 'products.csv';
+let inventory = 'inventory.csv';
 
 // Middleware
 // stuff that runs before the request is handled
@@ -10,9 +15,25 @@ app.use(express.json()) // to parse the JSON body
 // sends 'Pong' if successful connection
 app.get('/ping', (req, res) => res.send('Pong'))
 
-
 app.post('/signup', (req, res) => {
   const data = req.body;
+  
+  function onSubmit() {
+    // Gather input values
+    const user = `${fname.value}, ${lname.value}, ${email.value}, ${password.value}, ${role.value}\n`;
+    // import the append functionality from file system
+    const fs = require('fs');
+
+    // Append the data to the CSV files
+    appendFile(user, users, (err) => {
+      if (err) {
+        console.error('Error writing to file', err)
+      }
+      else {
+        console.log('User info successfully added')
+      }
+    })
+  }
 
 
   res.send({ Success: 'This server is working' }) // sends message to the console of server running
